3989ce4bfb58805e3b444816d2a31c92ac2141e1
Evandro2270 <evandro535@gmail.com>
2024-12-05 14:27:55 -0300

    Adicionar arquivos de projeto.

---
+++ ExerciciosCondicional3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+                CultureInfo CI = CultureInfo.InvariantCulture;
+
+               string[] vet = Console.ReadLine().Split(' ');
+               int x = int.Parse(vet[0]);
+               int y = int.Parse(vet[0]);
+
+
+                if (x % y == 0 || y % x == 0)
+                {
+                    Console.WriteLine("São multiplos");
+                }
+                else
+                {
+                    Console.WriteLine("Não multiplos");
+                }
+            }
+        }
+    }
+}
